c5f0d940c5ba0f6d97d66e9b40d2f32b9ef8f4c4
Amit Assaraf <amit@landa.app>
2020-03-23 01:01:18 +0200

    Added namespace disclaimer

--- src/drf_json_api_utils/namespace.py
+++ src/drf_json_api_utils/namespace.py
@@ -1,7 +1,22 @@
 #
+# ###################################################################################################################
+#  ______   _______        __    _  _______  _______        ______   _______  ___      _______  _______  _______
+# |      | |       |      |  |  | ||       ||       |      |      | |       ||   |    |       ||       ||       |
+# |  _    ||   _   |      |   |_| ||   _   ||_     _|      |  _    ||    ___||   |    |    ___||_     _||    ___|
+# | | |   ||  | |  |      |       ||  | |  |  |   |        | | |   ||   |___ |   |    |   |___   |   |  |   |___
+# | |_|   ||  |_|  |      |  _    ||  |_|  |  |   |        | |_|   ||    ___||   |___ |    ___|  |   |  |    ___|
+# |       ||       |      | | |   ||       |  |   |        |       ||   |___ |       ||   |___   |   |  |   |___
+# |______| |_______|      |_|  |__||_______|  |___|        |______| |_______||_______||_______|  |___|  |_______|
 #
+# ###################################################################################################################
 #
+# This library uses reflection to generate all the boilerplate types that DRF needs in order to create views. This is
+# the path where all the reflected types are saved at, meaning when referencing them they will show as a full path
+# of `drf_json_api_utils.namespace.[REFLECTED_TYPE_NAME_HERE]`.
 #
+# Read about reflection here: https://en.wikibooks.org/wiki/Python_Programming/Reflection
+#
+
 from typing import Type
 
 
