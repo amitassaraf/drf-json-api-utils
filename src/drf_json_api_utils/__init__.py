857a6a5a28109ce07f6f919a0ea68fac5968e680
Amit Assaraf <amit@landa.app>
2020-03-23 12:06:40 +0200

    [Ver bump]

--- src/drf_json_api_utils/__init__.py
+++ src/drf_json_api_utils/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'DRF Json Api Utils'
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 __author__ = 'Amit Assaraf'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2020 Amit Assaraf'
