ca61b7e9e53938d1686f27530eebc76c19757f1b
Amit Assaraf <amit@landa.app>
2020-06-30 18:44:49 +0300

    Ver bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='drf-json-api-utils',
-    version='1.3.9',
+    version='1.4.0',
     description='Utilities to reduce the boiler-plating of django-rest-framework-json-api',
     long_description=long_description,
     long_description_content_type='text/markdown',
