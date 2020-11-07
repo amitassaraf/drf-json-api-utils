b1b5b5d24b852222e7f0031fd9bc6447f0eb02c7
Amit Assaraf <amit@landa.app>
2020-11-08 00:05:19 +0200

    Revert "Small API Version Fixes"
    This reverts commit bd38f4349fd55a62df1c0cc92ebe59958f11cabe.

--- src/drf_json_api_utils/types.py
+++ src/drf_json_api_utils/types.py
@@ -1,17 +1,7 @@
-from dataclasses import dataclass
-from typing import Type
-
 from recordclass import recordclass
 
 Filter = recordclass('Filter', 'field lookups transform_value')
 ComputedFilter = recordclass('ComputedFilter', 'field filter_type filter_func')
 Relation = recordclass('Relation', 'field resource_name many primary_key_name required api_version')
-GenericRelation = recordclass('GenericRelation', 'field related many required')
+GenericRelation = recordclass('GenericRelation', 'field related many required api_version')
 CustomField = recordclass('CustomField', 'name callback')
-
-
-@dataclass
-class RelatedResource:
-    resource_name: str
-    model: Type
-    api_version: str = ''
