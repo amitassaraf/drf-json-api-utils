f268a27c0557ea8504b7863cd9550324fb0f4ec3
Guy Eshet <guy@landa.app>
2021-06-27 13:54:26 +0300

    added request to after serializtion

--- src/drf_json_api_utils/common.py
+++ src/drf_json_api_utils/common.py
@@ -14,4 +14,17 @@ class JsonApiSearchFilter(SearchFilter):
     search_param = 'filter[search]'
 
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class JsonApiGlobalSettings(metaclass=Singleton):
+    pass
+
+
 LOGGER = logging.getLogger(__name__)
