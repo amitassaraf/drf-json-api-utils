7c7e62bb65477cab42c1d610abb9216ef493e036
Eden Eliel <eden@landa.app>
2021-10-11 15:54:17 +0300

    add handler for exceptions

--- src/drf_json_api_utils/common.py
+++ src/drf_json_api_utils/common.py
@@ -1,9 +1,22 @@
 import logging
-
+from functools import wraps
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
+from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework_json_api.pagination import JsonApiPageNumberPagination
 
 DEFAULT_PAGE_SIZE = 50
+
+
+def exception_handler(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            return Response(data={'message': str(exc)},
+                            status=getattr(exc, 'http_status', HTTP_500_INTERNAL_SERVER_ERROR))
+    return wrapper
 
 
 class LimitedJsonApiPageNumberPagination(JsonApiPageNumberPagination):
